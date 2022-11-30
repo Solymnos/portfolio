858bdf749e628a3ccb6e653dc188cb7c8b1e2fde
carl ameline <solymnos@gmail.com>
2022-11-30 23:55:19 +0100

    redo home view, try be more responsive, didn't work, sad :(

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -6,31 +6,37 @@ function HomeView() {
   return (
     <div className='HomeView'>
         <TopBanner />
-        <div className='LeftPart'>
+        <div className='LeftPanel'>
             <div className='Content'>
-                <div className='Name'>
-                    CARL AMELINE
-                </div>
-                <div className='Subtitle'>Développeur fullstack, travaillant actuellement à IIDRE, à la recherche de nouvelles opportunités !
-                </div>
-            </div>   
+            <div className='Name'>
+                CARL AMELINE
+            </div>
+            <div className='Subtitle'>
+                Développeur fullstack, travaillant actuellement à IIDRE, à la recherche de nouvelles opportunités !
+            </div>
+            </div>
         </div>
-        <div className='RightPart'>
+        <div className='RightPanel'>
             <div className='Content'>
-                <div>
-                    Profil
+                <div className='Menu'>
+                    <h1>Profil</h1>
+                    <h2>01</h2>
                 </div>
-                <div>
-                    Parcours
+                <div className='Menu'>
+                    <h1>Parcours</h1>
+                    <h2>02</h2>
                 </div>
-                <div>
-                    Compétences
+                <div className='Menu'>
+                    <h1>Compétences</h1>
+                    <h2>03</h2>
                 </div>
-                <div>
-                    Projets
+                <div className='Menu'>
+                    <h1>Projets</h1>
+                    <h2>04</h2>
                 </div>
-                <div>
-                    Contacts
+                <div className='Menu'>
+                    <h1>Contacts</h1>
+                    <h2>05</h2>
                 </div>
             </div>
         </div>
