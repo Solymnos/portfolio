a39d7f15d1cfd658ecf8979db517bd186000d316
Corentin <solymnos@gmail.com>
2022-12-02 10:22:38 +0100

    add cv logo + links

--- src/components/ListLogo.jsx
+++ src/components/ListLogo.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import logoLinkedin from '../assets/img/logoLinkedin.png';
-import logoGithub from '../assets/img/logoGithub.png'
+import logoGithub from '../assets/img/logoGithub.png';
+import logoCV from '../assets/img/logoCV.png';
 
 function ListLogo() {
   return (
     <div className='ListLogo'>
-        <img src={logoLinkedin} alt='logo linkedin'/>
-        <img src={logoGithub} alt='logo linkedin'/>
-        <img src={logoLinkedin} alt='logo linkedin'/>
+        <a href='https://fr.linkedin.com/in/carl-ameline-47a087154' target='_blank' rel='noreferrer'>
+            <img src={logoLinkedin} alt='logo linkedin'/>
+        </a>
+        <a href='https://github.com/solymnos' target='_blank' rel='noreferrer'>
+            <img src={logoGithub} alt='logo Github'/>
+        </a>
+        <a href='/CV_Carl_Ameline.pdf' download>
+            <img src={logoCV} alt='logo CV'/>
+        </a>
     </div>
   )
 }
