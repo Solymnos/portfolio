2c448135f9078b62cf5e6953a893a71de2e8ee69
Corentin <solymnos@gmail.com>
2022-12-13 15:40:30 +0100

    manage flexbox in project and create a project data to display info, now need to have data change when slider is trigger

--- src/views/ProjectsView.js
+++ src/views/ProjectsView.js
@@ -4,6 +4,12 @@ import Toolbar from '../components/Toolbar';
 import AwesomeSlider from 'react-awesome-slider';
 import Style from'react-awesome-slider/dist/styles.css';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styled/open-animation/open-animation.scss';
+import projectsIcon from '../assets/img/projectsIcon.png';
+
+const projects =
+[
+  
+]
 
 function ProjectsView() {
   return (
@@ -12,13 +18,28 @@ function ProjectsView() {
       <div className='ProjectsViewTitle'>
         <h1>Projets</h1>
       </div>
-      <div className='LeftPanel'>
-        <AwesomeSlider cssModule={[AwesomeSliderStyles, Style]} bullets={false}>
-          <div className='Slide'>1</div>
-          <div className='Slide'>2</div>
-          <div className='Slide'>3</div>
-          <div className='Slide'>4</div>
-        </AwesomeSlider>
+      <div className='Content'>
+        <div className='LeftPanel'>
+          <AwesomeSlider cssModule={[AwesomeSliderStyles, Style]} bullets={false}>
+            <div className='Slide'>
+              <img src={projectsIcon} alt='icone'/>
+            </div>
+            <div className='Slide'>
+              <img src={projectsIcon} alt='icone'/>
+            </div>
+            <div className='Slide'>
+              <img src={projectsIcon} alt='icone'/>
+            </div>
+            <div className='Slide'>
+              <img src={projectsIcon} alt='icone'/>
+            </div>
+          </AwesomeSlider>
+        </div>
+        <div className='RightPanel'>
+          <div className='ProjectData'>
+            <h1>> Solary Live Extension</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
