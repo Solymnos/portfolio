8ef1741d2c68677ddd792ec80dbbc2e8ab6dacad
Corentin <solymnos@gmail.com>
2022-12-13 12:43:33 +0100

    add awesome slider on project part, need figure how to customize it

--- src/views/ProjectsView.js
+++ src/views/ProjectsView.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/ProjectsView.css';
 import Toolbar from '../components/Toolbar';
+import AwesomeSlider from 'react-awesome-slider';
+import Style from'react-awesome-slider/dist/styles.css';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styled/open-animation/open-animation.scss';
 
 function ProjectsView() {
   return (
@@ -8,6 +11,14 @@ function ProjectsView() {
       <Toolbar />
       <div className='ProjectsViewTitle'>
         <h1>Projets</h1>
+      </div>
+      <div className='LeftPanel'>
+        <AwesomeSlider cssModule={[AwesomeSliderStyles, Style]} bullets={false}>
+          <div className='Slide'>1</div>
+          <div className='Slide'>2</div>
+          <div className='Slide'>3</div>
+          <div className='Slide'>4</div>
+        </AwesomeSlider>
       </div>
     </div>
   )
