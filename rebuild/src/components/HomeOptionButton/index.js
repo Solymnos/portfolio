961b68f1ea3b6e7cde2e8e560bc4e0c2bfbd668f
Corentin <solymnos@gmail.com>
2022-12-21 15:52:50 +0100

    add home option update, make it work, now need attribute the right functions

--- rebuild/src/components/HomeOptionButton/index.js
+++ rebuild/src/components/HomeOptionButton/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const HomeOptionButton = ({img}) =>
+const HomeOptionButton = ({img, action}) =>
 {
     return (
         <div className='flex'>
-            <img className='w-14 m-5' src={img} alt='sunicon'></img>
+            <img className='w-14 m-5' src={img} alt='icon' onClick={action}></img>
         </div>
     )
 }
--- rebuild/src/components/HomeOptions/index.js
+++ rebuild/src/components/HomeOptions/index.js
@@ -5,12 +5,22 @@ import SunIcon from '../../assets/imgs/SunIcon.png';
 import EnglishIcon from '../../assets/imgs/EnglishIcon.png';
 import FrenchIcon from '../../assets/imgs/FrenchIcon.png';
 
+const changeTheme = () =>
+{
+    console.log('changeTheme')
+}
+
+const changeLanguage = () =>
+{
+    console.log('changeLanguage');
+}
+
 const HomeOptions = () => 
 {
     return (
         <div className='flex flex-row w-fit absolute top-10 right-10'>
             {Options.map(option => 
-                <HomeOptionButton img={option.img}/>
+                <HomeOptionButton className='opacity-0' img={option.img} action={option.action} />
             )}
         </div>    
     )
@@ -19,6 +29,6 @@ const HomeOptions = () =>
 export default HomeOptions;
 
 const Options = [
-    { img : MoonIcon , function : {} },
-    { img : FrenchIcon , function : {} }
+    { img : MoonIcon , action : changeTheme },
+    { img : FrenchIcon , action : changeLanguage }
 ]
