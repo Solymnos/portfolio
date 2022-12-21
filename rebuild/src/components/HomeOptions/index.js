08b28c83df0c778ecff4058827c8185f2f0a3441
carl ameline <solymnos@gmail.com>
2022-12-21 23:53:54 +0100

    add language change on rebuild but not working, todo tomorrow

--- rebuild/src/components/HomeOptions/index.js
+++ rebuild/src/components/HomeOptions/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HomeOptionButton from '../HomeOptionButton';
 import MoonIcon from '../../assets/imgs/MoonIcon.png';
 import SunIcon from '../../assets/imgs/SunIcon.png';
 import EnglishIcon from '../../assets/imgs/EnglishIcon.png';
 import FrenchIcon from '../../assets/imgs/FrenchIcon.png';
 
-const changeTheme = () =>
-{
-    console.log('changeTheme')
-}
-
-const changeLanguage = () =>
-{
-    console.log('changeLanguage');
-}
-
 const HomeOptions = () => 
 {
+    const [ThemeImg, setThemeImg] = useState(MoonIcon);
+    const [LanguageImg, setLanguageImg] = useState(FrenchIcon);
+    const [Language, setLanguage] = useState('fr');
+
+    const changeTheme = () =>
+    {
+        if (ThemeImg === MoonIcon)
+        {
+            setThemeImg(SunIcon);
+        } else 
+        {
+            setThemeImg(MoonIcon);
+        }
+    }
+
+    const changeLanguage = () =>
+    {
+        let loc = "http://localhost:3000/";
+
+        if (LanguageImg === FrenchIcon)
+        {
+            setLanguageImg(EnglishIcon);
+            setLanguage('en');
+        } else 
+        {
+            setLanguageImg(FrenchIcon);
+            setLanguage('fr');
+        }
+    }
+
+    const Options = [
+        { img : ThemeImg , action : changeTheme },
+        { img : LanguageImg , action : changeLanguage }
+    ]
+
     return (
         <div className='flex flex-row w-fit absolute top-10 right-10'>
             {Options.map(option => 
@@ -28,7 +53,3 @@ const HomeOptions = () =>
 
 export default HomeOptions;
 
-const Options = [
-    { img : MoonIcon , action : changeTheme },
-    { img : FrenchIcon , action : changeLanguage }
-]
--- rebuild/src/pages/HomePage.js
+++ rebuild/src/pages/HomePage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import HomeMenuItem from '../components/HomeMenuItem';
 import HomeOptions from '../components/HomeOptions';
+import {useTranslation} from 'react-i18next';
 
 const HomePage = () => 
 {
+    const { t } = useTranslation();
+
     return (
         <React.Fragment>
             <div className='w-screen h-screen grid grid-rows-2 xl:grid-cols-2 bg-bgDark'>
                 <div className='w-full h-full bg-bgDark xl:h-screen text-primary centered flex-col'>
                     <h1 className='text-9xl ml-32 font-bold'>CARL AMELINE</h1>
-                    <h2 className='text-secondary text-4xl ml-32 mt-14'>Ingénieur fullstack, travaillant actuellement pour IIDRE, et à la recherche de nouvelles opportunités de travail !</h2>
+                    <h2 className='text-secondary text-4xl ml-32 mt-14'>{t('Ingénieur fullstack, travaillant actuellement pour IIDRE, et à la recherche de nouvelles opportunités de travail !')}</h2>
                 </div>
                 <div className='w-full h-full bg-bgDark xl:h-screen text-primary flex-col pl-32 pr-44 flex justify-center text-right'>
                     {MenuItems.map(item => <HomeMenuItem name={item.name} url={item.url} id={item.id}/>)}
