7877c76254928b5db57a8c7e3f95430336337afd
Solymnos <solymnos@gmail.com>
2023-03-03 16:01:54 +0100

    finally ending project part

--- rebuild/src/pages/ProjectsPage.js
+++ rebuild/src/pages/ProjectsPage.js
@@ -94,7 +94,7 @@ const Projects =
   { 
     name : 'Projet 7',  
     desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae nulla eget ipsum viverra sodales porttitor quis mi. Proin nunc magna, sollicitudin ut augue vel, pharetra ullamcorper massa. Suspendisse ac aliquet libero. Morbi sit amet tellus laoreet, aliquet elit nec, convallis elit. Duis consequat consequat magna at gravida. Morbi vel massa rhoncus, tempus elit eget, suscipit dui. Donec sodales elementum felis a accumsan. Cras id urna nec urna lobortis ultricies. Nullam eleifend diam sit amet vulputate pulvinar. Nullam sed venenatis risus. Aliquam venenatis quam ante, ac dictum nisl mattis a. Mauris eu magna est. Donec eget risus eros. Lorem ipsum.', 
-    tech : ['C', 'C++','Python', 'javascript' ],
+    tech : ['C', 'C++','Python', 'javascript', 'C', 'C++','Python', 'javascript', 'C', 'C++','Python', 'javascript' ],
     link : [{
       icon : GithubIcon, 
       url :'https://google.com',
@@ -112,12 +112,50 @@ const Projects =
     }],
     icon : ProjectsIcon,
     id : 8
-  }
+  },
 ]
 
 const ProjectsPage = () => {
 
     const [projectIndex, setProjectIndex] = useState(0);
+    const [isInBottom, setIsInBottom] = useState(false);
+    const [isInTop, setIsInTop] = useState(true);
+
+    function handleDivScroll(e) {
+      let element = e.target;
+
+      console.log(element.scrollHeight);
+      console.log(element.scrollTop);
+      console.log(element.clientHeight);
+      console.log(element.scrollBottom);
+
+      if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+        setIsInBottom(true);
+      } else {
+        setIsInBottom(false);
+      }
+      if (element.scrollTop === 0)
+      {
+        setIsInTop(true);
+      } else {
+        setIsInTop(false);
+      }
+    }
+
+    var bottom;
+    var top;
+
+    if (!isInTop) {
+      top = <div className='flex text-center items-center mx-auto text-7xl text-primary'>↑</div>
+    } else {
+      top = <div className='flex text-center items-center mx-auto text-7xl invisible'>↑</div>
+    }
+    
+    if (!isInBottom) {
+      bottom = <div className='flex text-center items-center mx-auto text-7xl text-primary'>↓</div>
+    } else {
+      bottom = <div className='flex text-center items-center mx-auto text-7xl invisible'>↓</div>  
+    }
 
     return (
       
@@ -127,8 +165,61 @@ const ProjectsPage = () => {
         </div>
         <div class="flex justify-center flex-row items-center flex-grow">
           <div className='flex w-full flex-grow h-full'>
-            <div className='flex flex-grow w-2/5 flex-col h-full items-center'>
-              <div class='flew w-4/5 h-4/5  overflow-y-auto'>
+            <div className='flex h-full w-2/5 flex-grow flex-col '>
+              {top}
+              <div className='flex h-80 w-4/5 mx-auto flex-col overflow-scroll' onScroll={handleDivScroll}>
+                {Projects.map(project =>
+                <>
+                  {
+                    project.id === projectIndex ? (
+                      <div className='bg-primary' onClick={() => setProjectIndex(project.id)}>
+                        <h1 className='text-bgDark text-5xl my-4'>{project.name}</h1>
+                      </div>
+                    ) : (
+                      <div onClick={() => setProjectIndex(project.id)}>
+                        <h1 className='text-primary text-5xl my-4'>{project.name}</h1>
+                      </div>
+                    )
+                  }
+                </>
+                )}
+              </div>
+              {bottom}
+            </div>
+            <div className='flex h-full w-3/5 flex-grow flex-col'>
+              <div className='flex flex-row items-center'>
+                <h1 className='text-secondary text-5xl m-4 uppercase underline decoration-primary decoration-4'>{Projects[projectIndex].name}</h1>
+                <div className='flex grow mr-16'>
+                  {Projects[projectIndex].link.map(link =>
+                    <img className='h-12 ml-8' src={link.icon} alt='icon of the link'/>
+                  )}
+                </div>
+              </div>
+              <div className='flex grow flex-row'>
+                <p className='text-secondary'>
+                  {Projects[projectIndex].desc}
+                </p>
+                <img className='h-40 my-12 ml-12 mr-24' src={Projects[projectIndex].icon} alt='project icon'/>
+              </div>
+              <div className='mt-4 flex grow flex-row w-3/4 overflow-y-scroll'>
+                  {Projects[projectIndex].tech.map(tech =>
+                  <div className='h-min mx-2 p-3 flex border-4 border-primary'>
+                    <h1 className='text-primary text-2xl'>{tech}</h1>
+                  </div>
+                  )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default ProjectsPage;
+
+/*
+<div className='h-3/5 flex flex-grow w-2/5 flex-col items-center min-h-0'>
+              <div class='flex flex-col w-4/5 h-2/5  overflow-y-scroll'>
               {Projects.map(project =>
                   <div onClick={() => setProjectIndex(project.id)}>
                     <h1 className='text-primary text-5xl my-4'>{project.name}</h1>
@@ -136,96 +227,7 @@ const ProjectsPage = () => {
                 )}
               </div>
             </div>
-            <div className='bg-yellow-200 flex flex-grow w-3/5 h-full'>
+            <div className='bg-yellow-200 flex flex-grow w-3/5'>
               {projectIndex}
             </div> 
-          </div>
-        </div>
-      </div>
-       /* <div>
-            
-            <div class="bg-bgDark flex flex-col h-screen w-screen">
-                <Toolbar />
-                <div className='text-center pt-24 pb-24'>
-                    <h1 className='text-primary text-8xl font-bold'>Projets</h1>
-                </div>
-                <div className='h-full grid grid-cols-2'>
-                    <div className='h-full ml-36'>
-                        <AwesomeSlider cssModule={AwesomeSliderStyle} bullets={false} onTransitionStart={onTransition}>
-                            {Projects.map(project =>
-                                <div className=' bg-bgDark h-full w-full flex'>
-                                    <img className='m-auto w-60' src={project.icon} alt='icon of the project'/>
-                                </div>
-                            )}
-                        </AwesomeSlider>
-                    </div>
-                    <div className='bg-bgDark h-full'>
-                        <ProjectData project={Projects[projectIndex]}/>
-                    </div>
-                </div>   
-            </div>
-        </div>*/
-    )
-}
-
-export default ProjectsPage;
-//https://stackoverflow.com/questions/70162754/how-to-make-a-scrollable-section-inside-div-that-fills-in-all-the-remaining-spac
-/*
-
- {Projects.map(project =>
-                <div className=' active:bg-green-400' onClick={() => setProjectIndex(project.id)}>
-                  <h1 className='text-primary text-5xl my-5'>{project.name}</h1>
-                </div>
-                )}
-                */
-
-// <div class="flex-1 w-2/3 mx-auto p-4 text-lg  h-full shadow-lg bg-green-400">
-
-/*<div className='bg-bgDark w-screen h-screen'>
-                <div className='pt-24 pb-24 bg-bgDark text-center ml-auto mx-auto'>
-                    <h1 className='text-primary text-8xl font-bold'>Projets</h1>
-                </div>
-                <div className='bg-bgDark grid grid-cols-2 w-full'>
-                    <div className='bg-plum grow h-full'>
-                        oui ouo
-                    </div>
-                </div>
-            </div>*/
-
-/**
- * <Toolbar />
-            <div className='pt-24 pb-24 bg-bgDark text-center ml-auto mx-auto'>
-                <h1 className='text-primary text-8xl font-bold'>Projets</h1>
-            </div>
-            <div className='bg-bgDark grid grid-cols-2'>
-                <div className='w-full h-full bg-plum'>
-                    oui ouo
-                </div>
-            </div>
- */
-
-/*
-<Swiper
-              slidesPerView={1}
-              spaceBetween={30}
-              loop={true}
-              pagination={{
-                clickable: true,
-              }}
-              navigation={true}
-              modules={[Pagination, Navigation]}
-              onSlideChange={() => {
-                console.log('slide change')
-                
-              }}
-              onActiveIndexChange={(swiperCore) => {
-                console.log(swiperCore.activeIndex);
-              }}
-              className="mySwiper">
-                {Projects.map(project =>
-                  <SwiperSlide>
-                    <h1>{project.name}</h1>
-                  </SwiperSlide>
-                  )}
-              </Swiper>
-              */
+*/ 
